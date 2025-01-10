bebf4073f221f713622018f69303f9ea1c60eebb
liuzhe <liuzhe@qq.com>
2025-01-10 23:17:57 +0800

    fix

---
+++ src/pages/main/index.ts
@@ -0,0 +1,7 @@
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+    setup(){
+        console.log('欢迎使用小哲自制脚手架')
+    }
+})
--- src/pages/main/index.vue
+++ src/pages/main/index.vue
@@ -1,3 +1,5 @@
 <template>
-	<div>111</div>
+	<h2>欢迎使用小哲自制脚手架(vue3+ts)</h2>
 </template>
+
+<script lang="ts" src="./index.ts"></script>
